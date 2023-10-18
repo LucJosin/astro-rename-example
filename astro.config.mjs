b6b985f51f6e4ee71f5c9534445a013cd80269f5
Lucas Josino <contact@lucasjosino.com>
2023-10-18 20:00:09 -0300

    feat: Add astro-rename and HashRenamer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,19 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import rename from "astro-rename";
+
+import HashRenamer from "./src/lib/hash-renamer";
+
+const renamer = new HashRenamer();
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  integrations: [
+    rename({
+      rename: {
+        strategy: (key) => renamer.rename(key),
+      },
+    }),
+  ],
+});
